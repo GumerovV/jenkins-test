e4e73691a8d837b010239cd5949f4f4f42e89629
Владислав Гумеров <gumerovv@MacBook-Air-Vladislav.local>
2026-02-27 18:58:21 +0700

    feat: change index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/ping', (req, res) => {
-    res.json({ message: 'pong' });
+    res.json({ message: 'pong (jenkins)' });
 });
 
 app.use('/api/auth-service', createProxyMiddleware({
